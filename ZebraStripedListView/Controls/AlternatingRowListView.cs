c67f0ff061fee68a46113680f869db8cc9d3761d
Ben Dewey <bdewey01@hotmail.com>
2014-10-22 10:53:22 -0400

    Initial Commit

---
+++ ZebraStripedListView/Controls/AlternatingRowListView.cs
@@ -0,0 +1,45 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
+
+namespace ZebraStripedListView.Controls
+{
+	public class AlternatingRowListView : ListView
+	{
+		public static readonly DependencyProperty OddRowBackgroundProperty =
+			DependencyProperty.Register("OddRowBackground", typeof(Brush), typeof(AlternatingRowListView), null);
+		public Brush OddRowBackground
+		{
+			get { return (Brush)GetValue(OddRowBackgroundProperty); }
+			set { SetValue(OddRowBackgroundProperty, (Brush)value); }
+		}
+		
+		public static readonly DependencyProperty EvenRowBackgroundProperty =
+			DependencyProperty.Register("EvenRowBackground", typeof(Brush), typeof(AlternatingRowListView), null);
+		public Brush EvenRowBackground
+		{
+			get { return (Brush)GetValue(EvenRowBackgroundProperty); }
+			set { SetValue(EvenRowBackgroundProperty, (Brush)value); }
+		}
+	
+		protected override void PrepareContainerForItemOverride(DependencyObject element, object item)
+		{
+			base.PrepareContainerForItemOverride(element, item);
+			var listViewItem = element as ListViewItem;
+			if (listViewItem != null)
+			{
+				var index = IndexFromContainer(element);
+	
+				if ((index + 1) % 2 == 1)
+				{
+					listViewItem.Background = OddRowBackground;
+				}
+				else
+				{
+					listViewItem.Background = EvenRowBackground;
+				}
+			}
+			
+		}
+	}
+}
